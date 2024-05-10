b037cbc74e47145305a40f273ab9b48a0841219c
AmmArm14 <amarm2921@gmail.com>
2024-05-10 20:45:33 +0300

    one Comment Added to Java Program

---
+++ Final-project.java
@@ -0,0 +1,8 @@
+public class SimpleJavaClass 
+ { 
+  public static void main(String[] args) 
+   { 
+   System.out.println("Hello World!");
+   System.out.println("This is an edit for the code");
+   }
+ }
